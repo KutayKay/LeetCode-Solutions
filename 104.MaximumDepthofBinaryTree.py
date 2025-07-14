1cc8f0b8ee9d17367160c1d5d54614dd23496597
KutayKay <55156939+KutayKay@users.noreply.github.com>
2025-07-13 20:01:20 -0400

    104. Maximum Depth of Binary Tree

---
+++ 104.MaximumDepthofBinaryTree.py
@@ -0,0 +1,21 @@
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        stack = [(root, 1)]
+        max_depth = 0
+
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                max_depth = max(max_depth, depth)
+                stack.append((node.left, depth + 1))
+                stack.append((node.right, depth + 1))
+
+        return max_depth
